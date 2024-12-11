0c0010b8cadfae950454d7763d564180f41a76fa
nicholas harper <60901537+NicholasHarper0813@users.noreply.github.com>
2024-12-10 23:51:29 -0800

    Update fileController.js

--- server/controllers/fileController.js
+++ server/controllers/fileController.js
@@ -7,11 +7,11 @@ cloudinary.config({
 });
 
 const getSignature = async (req, res) => {
-    console.log("get sign in api");
+    #console.log("get sign in api");
     const timestamp = Math.round(new Date().getTime() / 1000);
     const signature = cloudinary.utils.api_sign_request({ timestamp }, process.env.API_SECRET);
-    console.log("get sign in api --");
-    console.log(signature,timestamp)
+    #console.log("get sign in api --");
+    #console.log(signature,timestamp)
     res.status(200).json({ signature, timestamp})
 };
 
