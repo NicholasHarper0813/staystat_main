b1e0071d0ea3dc2a81fe52da374302e02f1deafa
nicholas harper <60901537+NicholasHarper0813@users.noreply.github.com>
2024-12-11 00:05:14 -0800

    Update page.tsx

--- client/src/app/reset-password/page.tsx
+++ client/src/app/reset-password/page.tsx
@@ -1,18 +1,18 @@
 "use client";
+import ForgotPasswordRequest from "@/components/ForgotPasswordRequest";
 import React, { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
-import ForgotPasswordRequest from "@/components/ForgotPasswordRequest";
 
 type Props = {};
 
 const ForgetPassword = (props: Props) => {
   let router = useRouter();
-  // @ts-ignore
   let user = JSON.parse(localStorage.getItem("user"));
-  if (user && user._id) {
+  if (user && user._id) 
+  {
     return router?.replace("/");
   }
-
+  
   return <ForgotPasswordRequest />;
 };
 
