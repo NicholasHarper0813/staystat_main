b3d5bfdf740b67aec646fa7a4e5eb195a46442a4
nicholas harper <nicholasharper000@gmail.com>
2024-12-18 03:16:46 -0800

    Update authMiddleware.js

--- server/middlewares/authMiddleware.js
+++ server/middlewares/authMiddleware.js
@@ -3,22 +3,29 @@ const { User } = require("../models/userModel");
 
 const checkAuth = async (req, res, next) => {
   const { authtoken } = req.headers;
-  if (!authtoken) {
+  if (!authtoken) 
+  {
     return res.status(401).json({
       message: "You are not authorized or deactivated by admin",
     });
-  } else {
-    try {
+  } 
+  else 
+  {
+    try 
+    {
       const { id } = jwt.verify(authtoken, process.env.JWT_SECRET);
       const user = await User.findById(id);
-      if (!user || !user?.isActive) {
+      if (!user || !user?.isActive) 
+      {
         return res.status(201).json({
           message: "You are not authorized or deactivated by admin",
         });
       }
       req.user = user;
       next();
-    } catch (error) {
+    } 
+    catch (error) 
+    {
       return res.status(201).json({
         message: "You are not authorized or deactivated by admin",
       });
