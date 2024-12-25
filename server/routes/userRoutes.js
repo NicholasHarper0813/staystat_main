d4762327f97b13e2dcba2bc715d8667f159ba3b5
nicholas harper <nicholasharper000@gmail.com>
2024-12-24 17:16:44 -0800

    Update userRoutes.js

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,10 +1,10 @@
 const router = require("express").Router();
 const userController = require("../controllers/userController");
 
-router.post("/user/fetch-user", userController.getUser);
 router.get("/user/get-users", userController.getUsers);
 router.get("/user/get-users/search", userController.getUsersBySearch);
 router.get("/user/get-all-users", userController.getAllUsers);
+router.post("/user/fetch-user", userController.getUser);
 router.post("/user/create-user", userController.createUser);
 router.post("/user/update-user", userController.updateUser);
 router.post("/user/delete-user", userController.deleteUser);
