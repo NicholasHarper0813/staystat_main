d5364e9186c55cd89e7021f86aa6493027b85943
nicholas harper <nicholasharper000@gmail.com>
2024-12-24 17:15:59 -0800

    Update bookingsRoutes.js4

--- server/routes/bookingsRoutes.js
+++ server/routes/bookingsRoutes.js
@@ -3,10 +3,7 @@ const bookingController = require("../controllers/bookingsController");
 
 router.post("/booking/get-one-booking", bookingController.getBooking);
 router.post("/booking/get-all-bookings", bookingController.getAllBookings);
-router.get(
-  "/booking/get-all-bookings/search",
-  bookingController.getAllBookingsBySearch
-);
+router.get("/booking/get-all-bookings/search", bookingController.getAllBookingsBySearch);
 router.post("/booking/create-booking", bookingController.createBooking);
 router.post("/booking/update-booking", bookingController.updateBooking);
 router.post("/booking/cancel-booking", bookingController.cancelBooking);
