fc715af7a232fb30c970863a14671090c471d894
nicholas harper <nicholasharper000@gmail.com>
2024-12-24 17:17:06 -0800

    Update dbConnection.js

--- server/config/dbConnection.js
+++ server/config/dbConnection.js
@@ -2,14 +2,17 @@ const mongoose = require('mongoose');
 const colors = require("colors");
 
 const connectDB = async () => {
-    try {
+    try 
+    {
         const con = await mongoose.connect(process.env.MONGO_URI, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
         });
 
         console.log(`MongoDB Connected: ${con.connection.host}`.cyan.underline);
-    } catch (error) {
+    }
+    catch (error) 
+    {
         console.log(`Error: ${error.message}`.red.bold);
         process.exit();
     }
